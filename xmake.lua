c6ea8c8ed04913c71edf5a482aefd7d1580be974
Qudix <17361645+Qudix@users.noreply.github.com>
2024-10-11 16:32:30 -0500

    feat: use new default install/package in clib

--- xmake.lua
+++ xmake.lua
@@ -45,21 +45,5 @@ target("sfse-clean-main-menu")
     add_includedirs("src")
     set_pcxxheader("src/pch.h")
 
-    -- copy build to MODS or GAME paths
-    after_build(function(target)
-        local copy = function(env, ext)
-            for _, env in pairs(env:split(";")) do
-                if os.exists(env) then
-                    local plugins = path.join(env, ext, "SFSE/Plugins")
-                    os.mkdir(plugins)
-                    os.trycp(target:targetfile(), plugins)
-                    os.trycp(target:symbolfile(), plugins)
-                end
-            end
-        end
-        if os.getenv("XSE_SF_MODS_PATH") then
-            copy(os.getenv("XSE_SF_MODS_PATH"), target:name())
-        elseif os.getenv("XSE_SF_GAME_PATH") then
-            copy(os.getenv("XSE_SF_GAME_PATH"), "Data")
-        end
-    end)
+    -- add install files
+    add_installfiles("res/*.toml", { prefixdir = "SFSE/Plugins" })
